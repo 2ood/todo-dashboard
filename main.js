5eca65b08dc3fadefbbfda0e1d6698efddb1a37d
2ood <kyungmin.official0@gmail.com>
2022-03-21 15:45:15 +0900

    implemented dragstart, dragend, dragover

--- main.js
+++ main.js
@@ -16,23 +16,16 @@ const todo_ul = document.getElementById("todo-ul");
 const add = document.getElementById("add");
 const save = document.getElementById("save");
 const go = document.getElementById("go");
+const go_today = document.getElementById("go-today");
 const target_date_input = document.getElementById("target-date");
 
 target_date_input.value = getTodayDocName();
 initializeList(getTodayDocName());
 
+todo_ul.addEventListener("dragover",handleDrag);
 
 add.addEventListener("click",(evt)=>{
-  let new_li = document.createElement("li");
-  let input = document.createElement("input");
-  let cancel = document.createElement("div");
-  cancel.className= "cancel";
-  cancel.innerHTML = "X";
-  cancel.addEventListener("click",handleCancel);
-
-  new_li.appendChild(input);
-  new_li.appendChild(cancel);
-  todo_ul.appendChild(new_li);
+  todo_ul.appendChild(buildLi("",true));
 });
 
 
@@ -45,11 +38,19 @@ save.addEventListener("click",(evt)=>{
   targetRef = firestore.collection('todo').doc(target_date_input.value);
   targetRef.update({
     TODOS : contents_array
+  }).then(()=>{
+    save.innerHTML="saved!";
+    setTimeout(()=>{save.innerHTML="save";},2000);
   });
 });
 
 
 go.addEventListener("click",()=>{
+  initializeList(target_date_input.value);
+});
+
+go_today.addEventListener("click",()=>{
+  target_date_input.value = getTodayDocName();
   initializeList(target_date_input.value);
 });
 
@@ -62,13 +63,11 @@ function initializeList(docName) {
     if(doc.exists) {
       const arr = doc.data().TODOS;
       if(arr.length==0) {
-        todo_ul.innerHTML+= `
-        <li><input type="text" value="(None)" class="none"></input><div class="cancel">X</div></li>`;
-        todo_ul.querySelector("input.none:first-of-type").addEventListener("change",(evt)=>{evt.srcElement.classList.remove("none");});
+        let li = buildLi("(none)",true);
+        todo_ul.appendChild(li);
       }
       for(let i=0;i<arr.length;i++) {
-        todo_ul.innerHTML+= `
-        <li><input type="text" value="${arr[i]}"></input><div class="cancel">X</div></li>`;
+        todo_ul.appendChild(buildLi(arr[i],false));
       }
       cancels = todo_ul.getElementsByClassName("cancel");
       for(c of cancels) {
@@ -79,9 +78,8 @@ function initializeList(docName) {
         dateRef.set({
           TODOS :[]
         });
-          todo_ul.innerHTML+= `
-          <li><input type="text" value="(None)" class="none"></input><div class="cancel">X</div></li>`;
-          todo_ul.querySelector("input.none:first-of-type").addEventListener("change",(evt)=>{evt.srcElement.classList.remove("none");});
+        let li = buildLi("(none)",true);
+        todo_ul.appendChild(li);
       }
   });
 }
@@ -105,4 +103,63 @@ function handleCancel(evt) {
   const target = evt.srcElement.parentNode;
   console.log(target);
   target.parentNode.removeChild(target);
+}
+
+function buildLi(value, isNone) {
+  let new_li = document.createElement("li");
+  new_li.draggable = true;
+  new_li.className = "draggable";
+  new_li.addEventListener("dragstart",handleDragStart);
+  new_li.addEventListener("dragend",handleDragEnd);
+  let input = document.createElement("input");
+  input.value = value;
+  if(isNone) {
+    input.addEventListener("change",(evt)=>{evt.srcElement.classList.remove("none");});
+    input.className="none";
+  }
+  let cancel = document.createElement("div");
+  cancel.className= "cancel";
+  cancel.innerHTML = "X";
+  cancel.addEventListener("click",handleCancel);
+
+  new_li.appendChild(input);
+  new_li.appendChild(cancel);
+
+  return new_li;
+}
+
+function handleDragStart(evt) {
+  evt.srcElement.classList.add("dragging");
+  evt.srcElement.classList.remove("draggable");
+}
+function handleDragEnd(evt) {
+  evt.srcElement.classList.remove("dragging");
+  evt.srcElement.classList.add("draggable");
+}
+
+function handleDrag(evt) {
+  evt.preventDefault();
+  const afterElement = getDragAfterElement(todo_ul,evt.clientY);
+  const dragging = document.querySelector(".dragging");
+  todo_ul.insertBefore(dragging,afterElement);
+
+}
+
+function getDragAfterElement(ul, y) {
+  const draggable_elements = [...ul.querySelectorAll("li.draggable")];
+
+  let result = draggable_elements[0];
+  let closest = Number.NEGATIVE_INFINITY;
+
+  for(el of draggable_elements) {
+    const box = el.getBoundingClientRect();
+    const offset = y-box.top;
+    if(offset<=0 && offset >= closest) {
+      result = el;
+      closest = offset;
+    }
+    else ;
+  }
+
+  return result;
 }
